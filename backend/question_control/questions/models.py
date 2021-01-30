2f107eacf8868602d8f51bdde59acc1c53569de1
daisuke0207 <daisuke02074869@gmail.com>
2021-01-30 22:07:54 +0900

    User外部キーを紐付ける。

--- backend/question_control/questions/models.py
+++ backend/question_control/questions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Question(models.Model):
     question_text = models.CharField(
@@ -7,20 +9,24 @@ class Question(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f'{self.question_text[:100]}'
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.TextField(
         verbose_name='回答文',
-    ) 
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.answer_text[:200]
+
 
 class AnswerLike(models.Model):
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
@@ -30,3 +36,4 @@ class AnswerLike(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
