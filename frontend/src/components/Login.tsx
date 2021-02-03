91eafba4ca5bb95eadf45224017f6976321f263b
daisuke0207 <daisuke02074869@gmail.com>
2021-02-03 20:05:19 +0900

    Tokenによるユーザー認証
    Tokenを保存する。

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,46 +1,35 @@
 import React, {useState} from 'react'
-import axios from '../axios'
-
+import { asyncLogin, asyncRegister } from '../api/UserAPI'
 
 const Login: React.FC = () => {
   const [isLogin, setIsLogin] = useState(true)
   const [username, setUsername] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-  const postUri = isLogin ? `/auth/` : `/question/users/`
 
+  const authUser = (e: any) => {
+    e.preventDefault()
+    if (isLogin) {;
+      login()
+    }
+    else {
+      register()
+    }
+    resetForm()
+  }
+
+  const register = async () => {
+    await asyncRegister({ username: username, password: password })
+    login()
+  }
+
+  const login = async () => {
+    const result = await asyncLogin({ username: username, password: password })
+    localStorage.setItem("token", result.token)
+  }
 
   const changeFunc = () => {
     setIsLogin(!isLogin)
-    resetForm()
-  }
-
-  const postLogin = () => {
-    let data = new FormData()
-
-    data.append('username', username)
-    data.append('password', password)
-
-    axios.post(postUri, data)
-      .then(res => {alert(res.data)})
-      .catch(error => {alert(error.message)})
-
-    // TODO ページ遷移
-    resetForm()
-  }
-
-  const postRegister = () => {
-    let data = new FormData()
-
-    data.append('username', username)
-    data.append('email', email)
-    data.append('password', password)
-
-    axios.post(postUri, data)
-      .then(res=> {alert(res.data)})
-      .catch(error => {alert(error.message)})
-
-    // TODO ページ遷移
     resetForm()
   }
 
@@ -53,25 +42,25 @@ const Login: React.FC = () => {
   return (
     <div>
       <h1>{isLogin ? "ログイン" : "新規登録"}</h1>
-      <div>
-        <label htmlFor="name">ユーザー名<span>（必須）</span></label>
-        <input type="text" id="name" name="name" value={username} onChange={evt=> {setUsername(evt.target.value)}}></input>
-      </div>
-      {
-        isLogin ? null :
+      <form onSubmit={authUser}>
         <div>
-        <label htmlFor="email">メールアドレス<span>（任意）</span></label>
-        <input type="email" id="email" name="email" value={email} onChange={evt=>{setEmail(evt.target.value)}} ></input>
-      </div>
-      }
-      <div>
-        <label htmlFor="password">パスワード<span>（必須）</span></label>
-        <input type="password" id="password" name="password" value={password} onChange={evt=>{setPassword(evt.target.value)}}></input>
-      </div>
-      <div>
-        <button type="submit" onClick={isLogin ? postLogin : postRegister}>{isLogin ? 'ログイン' : "新規登録"}</button>
-      </div>
-      <button type="button" onClick={changeFunc}>{isLogin ? "新規登録へ" : "ログインへ"}</button>
+          <label>ユーザー名<span>（必須）</span>: </label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        {
+          isLogin ? null :
+          <div>
+            <label>メールアドレス<span>（任意）</span>: </label>
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+        }
+        <div>
+          <label>パスワード<span>（必須）</span>: </label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">{isLogin ? "ログイン" : "新規登録"}</button>
+        <button type="button" onClick={changeFunc}>{isLogin ? "新規登録へ" : "ログインへ"}</button>
+      </form>
     </div>
   )
 }
