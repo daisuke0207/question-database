eff2e69a7fb9d7f4d42bfa03e8fef979c0ad2a2c
daisuke0207 <daisuke02074869@gmail.com>
2021-02-05 12:16:07 +0900

    ユーザー情報取得するAsync関数の作成
    成功時 : ユーザーデータを返す。失敗時 : unidifinedを返す。

--- frontend/src/api/UserAPI.ts
+++ frontend/src/api/UserAPI.ts
@@ -3,10 +3,21 @@ import axios from '../const/axios'
 
 export const asyncRegister = async (auth: any) => {
   const res = await axios.post('/question/users/', auth)
-  return res.data.token
+  return res.data
 }
 
 export const asyncLogin = async (auth: any) => {
   const res = await axios.post('/auth/', auth)
   return res.data
+}
+
+export const asyncGetProfile = async () => {
+  const res = await axios.get('/question/profile/', {
+    headers: {
+      Authorization: `token ${localStorage.token}`
+    }
+  })
+  .then(res => { return res.data })
+  .catch(err => { return undefined })
+  return res
 }
