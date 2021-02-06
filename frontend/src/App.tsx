fe8ca67eb49053599626f5756b4039183e36b3fb
daisuke0207 <daisuke02074869@gmail.com>
2021-02-06 16:39:16 +0900

    UserContextの作成 及び 編集
    UserContextの定義。AppコンポーネントにUserのプロフィール情報取得。子コンポーネントのMainPageで使用する。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import Auth from './components/Auth'
 import MainPage from './components/MainPage'
+import { asyncGetProfile } from './api/UserAPI'
+import { UserContext }  from './contexts/UserContext'
 
 
 const App: React.FC = () => {
+  const [profile, setProfile] = useState<any>()
+
+  const userProfile = async () => {
+    const result = await asyncGetProfile()
+    setProfile(result)
+  }
+
+  useEffect(() => {
+    userProfile()
+  }, [])
 
   return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Auth} />
-          <Route exact path="/question" component={MainPage} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <UserContext.Provider value={profile}>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Auth} />
+            <Route exact path="/question" component={MainPage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </UserContext.Provider>
   )
 }
 
--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -1,45 +1,29 @@
-import React, {useState, useEffect} from 'react'
+import React, { useContext } from 'react'
 import { useHistory } from "react-router-dom";
-import { asyncGetProfile } from '../api/UserAPI';
 import Question from './Question';
-
+import { UserContext } from '../contexts/UserContext'
 
 const MainPage: React.FC = () => {
 
-  type PROFILE = {
-    id: number;
-    username: string;
-    email: string;
-  }
-
+  const profile = useContext(UserContext)
   const history = useHistory()
-  const [profile, setProfile] = useState<PROFILE>()
 
   const logout = () => {
     localStorage.removeItem("token")
     history.push("/")
   }
 
-  const userProfile = async () => {
-    const result = await asyncGetProfile()
-    setProfile(result)
-  }
-
-  useEffect(() => {
-    userProfile()
-  }, [])
-
   return (
-    <>
-      <div>
-        {profile !== undefined ? <div>{profile.id} : {profile.username}</div> : "未ログイン"}
-        <Question />
-        { profile !== undefined ?
-          <button onClick={logout}>ログアウト</button> :
-          <button onClick={() => history.push("/")}>ログイン</button>
-        }
-      </div>
-    </>
+      <>
+        <div>
+          {profile !== undefined ? <div>{profile.id} : {profile.username}</div> : "未ログイン"}
+          <Question />
+          { profile !== undefined ?
+            <button onClick={logout}>ログアウト</button> :
+            <button onClick={() => history.push("/")}>ログイン</button>
+          }
+        </div>
+      </>
   )
 }
 
