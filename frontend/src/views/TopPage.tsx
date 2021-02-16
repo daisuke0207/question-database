70c4efc12d778d3040d89e0b2fb82489d7cdc497
daisuke0207 <daisuke02074869@gmail.com>
2021-02-16 20:47:49 +0900

    Update

--- frontend/src/views/TopPage.tsx
+++ frontend/src/views/TopPage.tsx
@@ -1,8 +1,24 @@
 import React, { useContext } from 'react'
 import { useHistory } from "react-router-dom";
-import Answer from '../components/Answer';
 import Question from '../components/Question';
 import { UserContext } from '../contexts/UserContext'
+// import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+// const StyledButton = styled(Button)`
+//   background-color: #6772e5;
+//   color: #fff;
+//   box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+//   padding: 7px 14px;
+//   &:hover {
+//     background-color: #5469d4;
+//   }
+// `;
+
+// const TopPageHeader = styled.header`
+//   backgroud-color: #000
+//   color: #fff
+// `
 
 const MainPage: React.FC = () => {
 
@@ -17,14 +33,14 @@ const MainPage: React.FC = () => {
   return (
       <>
         <div>
-          {profile !== undefined ? <div>{profile.id} : {profile.username}</div> : "未ログイン"}
-          <Question />
-          <span>--------------------------------------------------</span>
-          <Answer />
-          { profile !== null ?
+          <header>
+            {profile !== undefined ? <div>{profile.id} : {profile.username}</div> : "未ログイン"}
+            { profile !== null ?
             <button onClick={logout}>ログアウト</button> :
             <button onClick={() => history.push("/")}>ログイン</button>
-          }
+            }
+          </header>
+          <Question />
         </div>
       </>
   )
