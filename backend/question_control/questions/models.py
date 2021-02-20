acc27ef26660bfed3d714586bdfa2cf8613b6ca9
daisuke0207 <daisuke02074869@gmail.com>
2021-02-20 13:39:30 +0900

    UserProfileテーブルの作成

--- backend/question_control/questions/models.py
+++ backend/question_control/questions/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class UserProfile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    GENDER_CHOICES = (
+        ('男', 'Male'),
+        ('女', 'Female'),
+        ('その他', 'Other'),
+    )
+
+    Overview_text = models.TextField(max_length=1000, verbose_name='概要')
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    occupation = models.CharField(max_length=100, verbose_name='職業')
+    icon = models.ImageField(max_length=100, upload_to='None', blank=True)
+
+
 class Question(models.Model):
     question_text = models.CharField(
         verbose_name='質問文',
